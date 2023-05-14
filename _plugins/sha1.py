474cfefda3d4a4c0bd888a97ed5d99ea51555c21
- <>
2023-05-14 10:15:27 +0200

    add

--- _plugins/sha1.py
+++ _plugins/sha1.py
@@ -3,4 +3,4 @@ import hashlib
 
 @obraz.template_filter('sha1')
 def sha1(content: str, config: str) -> str:
-    return str(hashlib.sha1(content.encode('utf-8')))
+    return hashlib.sha1(content.encode('utf-8')).hexdigest()
